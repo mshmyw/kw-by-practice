91e6e6a45acd7b890591fe8e16c8971db3d922b0
摘新 <shuanghua.msh@alibaba-inc.com>
2022-05-16 23:06:22 +0800

    feat(): 扇形

--- visual/canvas/index.js
+++ visual/canvas/index.js
@@ -24,16 +24,76 @@ const _createPolygon = (ctx, n, dx, dy, size) => {
 const createPolygon = () => {
   const ctx = getCtx();
   for(let i = 3; i < 10; i++) {
-    _createPolygon(ctx, i, (i - 2) * 100, 75, 40);
+    const n = i;
+    const dx = n > 6 ? (n - 5) * 100 : (n -2) * 100;
+    const dy = n > 6 ? 3*75 : 75;
+    _createPolygon(ctx, i, dx, dy, 40);
     ctx.fillStyle = 'HotPink';
     ctx.fill();
   }
 }
+
+/**
+ * 绘制圆角矩形
+ * width height 宽高
+ * r 圆角半径
+ * dx, dy 左上角顶点坐标
+ */
+const _createRoundedRect = (ctx, dx, dy, width, height, r) => {
+  ctx.beginPath();
+  ctx.moveTo(dx + r, dy);
+
+  ctx.lineTo(dx + width - r, dy);
+  ctx.arcTo(dx + width, dy, dx + width, dy + r, r);
+  
+  ctx.lineTo(dx + width, dy + height - r);
+  ctx.arcTo(dx + width, dy + height, dx + width - r, dy + height, r);
+
+  ctx.lineTo(dx + r, dy + height);
+  ctx.arcTo(dx, dy + height, dx, dy + height - r, r);
+
+  ctx.lineTo(dx, dy + r);
+  ctx.arcTo(dx, dy, dx + r, dy, r);
+
+  // ctx.closePath();
+};
+
+const createRoundedRect = () => {
+  const ctx = getCtx();
+  _createRoundedRect(ctx, 200, 300, 200, 200, 20);
+  ctx.strokeStyle = 'HotPink';
+  ctx.stroke();  
+}
+
+/**
+ * 绘制扇形
+ * dx dy 原点坐标
+ * r 圆角半径
+ * angle1 angle2 起始和结束角度
+ */
+const _createSector = (ctx, dx, dy, r, angle1, angle2) => {
+  ctx.beginPath();
+  ctx.moveTo(dx, dy);
+  ctx.arc(dx, dy, r, angle1 * Math.PI/180, angle2*Math.PI/180, false);
+  ctx.closePath();
+}
+
+const createSector = () => {
+  const ctx = getCtx();
+  _createSector(ctx, 300, 600, 100, 30, 120);
+  ctx.fillStyle = 'HotPink';
+  ctx.fill();
+};
+
 const main = () => {
   // 绘制多边形
   createPolygon();
   
-  // TODO 绘制圆角矩形 createRoundedRect
+  // 绘制圆角矩形 
+  createRoundedRect();
+
+  // 绘制扇形
+  createSector();
 
 }
 
